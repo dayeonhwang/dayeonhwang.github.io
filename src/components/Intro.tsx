5f82ce5e6e4f6f00b6f6a4825f3ed7fc3079edc1
Dayeon Hwang <dahwan@microsoft.com>
2025-03-09 22:18:09 -0700

    adjust spacing with logo

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,6 +4,7 @@ import { Page } from '../interfaces/page';
 import { setPage } from '../redux/actions';
 import { IntroStrings } from '../strings/strings';
 import '../styles/Intro.css';
+import logo from '../assets/logo.png'; // Import your logo
 
 const Intro: React.FC = () => {
     const dispatch = useDispatch();
@@ -13,10 +14,13 @@ const Intro: React.FC = () => {
     };
 
     return (
-        <>
-            <div className="title">{IntroStrings.title}</div>
-            <button className="coming-soon-button" onClick={handleButtonClick}>{IntroStrings.buttonText}</button>
-        </>
+            <div className="intro-container">
+                <div className="logo-container">
+                    <img src={logo} alt="Logo" className="intro-logo" />
+                </div>
+                <div className="title">{IntroStrings.title}</div>
+                <button className="coming-soon-button" onClick={handleButtonClick}>{IntroStrings.buttonText}</button>
+            </div>
     );
 };
 
